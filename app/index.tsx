e07c9d64fc4e644d740f9a945117ff4aebeb86fe
Mateus <mateuscaldarte@hotmail.com>
2024-09-09 11:26:37 -0300

    feat: first commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,33 @@
-import { Text, View } from "react-native";
+import React from "react";
+import { Text, View, StatusBar, Dimensions } from "react-native";
+import {
+  useFonts,
+  Roboto_700Bold,
+  Roboto_400Regular,
+} from "@expo-google-fonts/roboto";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
+  const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular });
+
+  const { height, width } = Dimensions.get("window");
+
   return (
-    <View
+    <SafeAreaView
       style={{
-        flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: "#202024",
+        height: height,
+        width: width,
       }}
     >
-      <Text>Olá SKD51</Text>
-    </View>
+      <StatusBar backgroundColor="transparent" translucent />
+      {fontsLoaded ? (
+        <Text style={{ color: "#fff" }}>Olá SKD51</Text>
+      ) : (
+        <View />
+      )}
+    </SafeAreaView>
   );
 }
